57b8a3e36d89a277b14eaad25523bbe2cd9abb66
volleyball26 <1056742786@qq.com>
2021-02-06 15:27:10 +0800

    stack

---
+++ data_structure/2.栈.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+
+class Stack:
+    # 实现栈的push、pop、 取栈顶 类
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def get_top(self):
+        if len(self.stack) > 0:
+            self.stack.pop(-1)
+        return None
